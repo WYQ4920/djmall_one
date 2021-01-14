ef72cb1652aab6c2c6e40bfbc42750bbaa5e935f
zjj <zjj961021@163.com>
2021-01-14 15:28:20 +0800

    增加静态资源，及用户实体类

---
+++ djmall_admin/src/main/java/com/dj/mall/auth/web/UserController.java
@@ -0,0 +1,15 @@
+package com.dj.mall.auth.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user/")
+@RestController
+public class UserController {
+
+
+
+
+
+
+}
--- djmall_provider/djmall_auth_service/src/main/java/com/dj/mall/auth/entity/UserEntity.java
+++ djmall_provider/djmall_auth_service/src/main/java/com/dj/mall/auth/entity/UserEntity.java
@@ -5,13 +5,30 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+/**
+ * 用户实体类
+ * @author dj
+ */
 @Data
-@TableName("user")
+@TableName("djmall_auth_user")
 public class UserEntity {
 
+    /**
+     * 用户ID
+     */
     @TableId(type = IdType.AUTO)
     private Integer id;
 
+    /**
+     * 用户名
+     */
     private String userName;
+
+    /**
+     * 用户密码
+     */
+    private String userPwd;
+
+
 
 }
