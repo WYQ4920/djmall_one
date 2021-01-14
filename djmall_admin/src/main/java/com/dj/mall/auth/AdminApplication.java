edf1380a680f4b5372c989bd6ca5bf6f05a59c37
zjj <zjj961021@163.com>
2021-01-14 18:04:33 +0800

    启动类增加开启过滤器注解

--- djmall_admin/src/main/java/com/dj/mall/auth/AdminApplication.java
+++ djmall_admin/src/main/java/com/dj/mall/auth/AdminApplication.java
@@ -3,6 +3,7 @@ package com.dj.mall.auth;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @Author zhengyk
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @EnableDubbo
+@ServletComponentScan("com.dj.mall.auth.config")
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
