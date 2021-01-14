5da19859b8072f34a1833e8e82cd1524f4ed00cf
zyk <zyk13994755749@163.com>
2021-01-14 16:43:07 +0800

    提交展示资源页面

--- djmall_admin/src/main/java/com/dj/mall/auth/web/ResourceController.java
+++ djmall_admin/src/main/java/com/dj/mall/auth/web/ResourceController.java
@@ -1,7 +1,14 @@
 package com.dj.mall.auth.web;
 
+import com.dj.mall.auth.api.ResourceApi;
+import com.dj.mall.auth.dto.ResourceDTO;
+import com.dj.mall.common.base.ResultModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Author zhengyk
@@ -10,5 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/res/")
 public class ResourceController {
+
+
 
 }
--- djmall_admin/src/main/java/com/dj/mall/auth/web/page/ResourcePageController.java
+++ djmall_admin/src/main/java/com/dj/mall/auth/web/page/ResourcePageController.java
@@ -2,9 +2,15 @@ package com.dj.mall.auth.web.page;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 @RequestMapping("/res/")
 public class ResourcePageController {
+
+    @RequestMapping("toShowResZtree")
+    public String toShowResZtree() throws Exception{
+        return "res/show";
+    }
 
 
 
