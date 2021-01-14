a37c702c494c0cb47249b229b96b74bc7ff0e84d
zjj <zjj961021@163.com>
2021-01-15 00:43:32 +0800

    用户 新增，修改

--- djmall_api/djmall_auth_api/src/main/java/com/dj/mall/auth/api/UserApi.java
+++ djmall_api/djmall_auth_api/src/main/java/com/dj/mall/auth/api/UserApi.java
@@ -8,5 +8,13 @@ public interface UserApi {
 
     UserDTO findUserByNameAndPwd(String userName, String userPwd) throws Exception;
 
-    List<UserDTO> findAll(UserDTO userDTO);
+    List<UserDTO> findAll(UserDTO userDTO) throws Exception;
+
+    void addUser(UserDTO userDTO) throws Exception;
+
+    UserDTO findUserById(Integer id) throws Exception;
+
+    void updateUser(UserDTO userDTO) throws Exception;
+
+    boolean checkUserName(String userName) throws Exception;
 }
