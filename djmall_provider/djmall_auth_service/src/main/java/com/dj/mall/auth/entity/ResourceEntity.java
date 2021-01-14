493803a8143dac657d0d0926b30836a73ce15c62
zyk <zyk13994755749@163.com>
2021-01-14 15:27:35 +0800

    提交静态资源

---
+++ djmall_provider/djmall_auth_service/src/main/java/com/dj/mall/auth/entity/ResourceEntity.java
@@ -0,0 +1,30 @@
+package com.dj.mall.auth.entity;
+
+/**
+ * @Author zhengyk
+ * @Date 2021/1/14 15:07
+ */
+public class ResourceEntity {
+
+    /**
+     * 资源ID
+     */
+    private Integer id;
+
+    /**
+     * 资源名
+     */
+    private String ResourceName;
+
+    /**
+     * 资源路径
+     */
+    private String url;
+
+    /**
+     * 父级ID
+     */
+    private Integer parentId;
+
+
+}
