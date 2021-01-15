da274811c64ce50e71690ac072111b31bcdde67e
zjt <zjt13643589167@163.com>
2021-01-15 12:05:50 +0800

    修改代码提交

--- djmall_admin/src/main/java/com/dj/mall/auth/web/page/ResourcePageController.java
+++ djmall_admin/src/main/java/com/dj/mall/auth/web/page/ResourcePageController.java
@@ -2,9 +2,7 @@ package com.dj.mall.auth.web.page;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.dj.mall.auth.api.ResourceApi;
-import com.dj.mall.auth.dto.ResourceDTO;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,23 +16,8 @@ public class ResourcePageController {
 
     @RequestMapping("toShowResZtree")
     public String toShowResZtree() throws Exception {
-        return "res/show";
+        return "index/left";
     }
-
-    @RequestMapping("toAdd")
-    public String toAdd(Integer parentId,Model model) throws Exception {
-        model.addAttribute("parentId",parentId);
-        return "res/add";
-    }
-
-    @RequestMapping("toUpdate")
-    public String toUpdate(Model model,Integer id) throws Exception {
-        ResourceDTO one = resourceApi.findById(id);
-        model.addAttribute("one",one);
-        return "res/update";
-    }
-
-
 
 }
 
