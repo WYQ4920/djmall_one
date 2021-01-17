3bc2b2d641d8f939d184899f8a59d8a0ee2aee10
wyq <17735988709@163.com>
2021-01-17 18:53:21 +0800

    权限控制-资源管理-资源ztree的展示、新增、修改、删除

---
+++ djmall_admin/src/main/java/com/dj/mall/auth/web/res/WyqResourceController.java
@@ -0,0 +1,74 @@
+package com.dj.mall.auth.web.res;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.dj.mall.auth.api.res.WyqResourceApi;
+import com.dj.mall.auth.dto.res.ResourceDTO;
+import com.dj.mall.auth.vo.ResourceVOReq;
+import com.dj.mall.auth.vo.ResourceVOResp;
+import com.dj.mall.common.base.ResultModel;
+import com.dj.mall.common.util.DozerUtil;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @Author WYQ
+ * @Date 2021/1/15 22:00
+ * 资源表 -> 控制层
+ */
+
+@RestController
+@RequestMapping("/auth/resource/wyq/")
+public class WyqResourceController {
+
+    @Reference
+    private WyqResourceApi wyqResourceApi;
+
+    /**
+     * 展示资源
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("list")
+    public ResultModel show() throws Exception {
+        List<ResourceDTO> resourceDTOList = wyqResourceApi.findResourceAllByName();
+        return new ResultModel().success(DozerUtil.mapList(resourceDTOList, ResourceVOResp.class));
+    }
+
+    /**
+     * 新增资源
+     */
+    @PostMapping("add")
+    public ResultModel add(ResourceVOReq resourceVOReq) throws Exception {
+        Assert.hasText(resourceVOReq.getResourceName(), "资源名不能为空");
+        Assert.hasText(resourceVOReq.getUrl(), "资源PATH不能为空");
+        Assert.hasText(resourceVOReq.getResourceCode(), "资源编码不能为空");
+        Assert.hasText(resourceVOReq.getResourceType(), "资源类型不能为空");
+        wyqResourceApi.addResource(DozerUtil.map(resourceVOReq, ResourceDTO.class));
+        return new ResultModel().success();
+    }
+
+    /**
+     * 修改资源
+     */
+    @PutMapping("update")
+    public ResultModel update(ResourceVOReq resourceVOReq) throws Exception {
+        Assert.hasText(resourceVOReq.getResourceName(), "资源名不能为空");
+        Assert.hasText(resourceVOReq.getUrl(), "资源PATH不能为空");
+        Assert.hasText(resourceVOReq.getResourceCode(), "资源编码不能为空");
+        Assert.hasText(resourceVOReq.getResourceType(), "资源类型不能为空");
+        wyqResourceApi.updateResource(DozerUtil.map(resourceVOReq, ResourceDTO.class));
+        return new ResultModel().success();
+    }
+
+    /**
+     * 删除资源
+     */
+    @PostMapping("del")
+    public ResultModel del(ResourceVOReq resourceVOReq) throws Exception {
+        wyqResourceApi.delResource(DozerUtil.map(resourceVOReq, ResourceDTO.class));
+        return new ResultModel().success();
+    }
+
+}
