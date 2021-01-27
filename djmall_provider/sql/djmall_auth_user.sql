e7161f7703389361b78a874c8d5ddb639aee2721
zjj <zjj961021@163.com>
2021-01-27 21:06:52 +0800

    更新数据库用户表

--- djmall_provider/sql/djmall_auth_user.sql
+++ djmall_provider/sql/djmall_auth_user.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2021-01-19 14:55:45
+Date: 2021-01-27 21:05:05
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,16 +27,14 @@ CREATE TABLE `djmall_auth_user` (
   `salt` varchar(255) NOT NULL COMMENT '密码盐',
   `user_phone` varchar(255) NOT NULL COMMENT '用户手机号',
   `user_email` varchar(255) NOT NULL COMMENT '电子邮箱',
-  `user_sex` int(11) NOT NULL COMMENT '用户性别：1为男，2为女',
+  `user_sex` varchar(255) NOT NULL COMMENT '用户性别',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='权限管理：用户表';
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='权限管理：用户表';
 
 -- ----------------------------
 -- Records of djmall_auth_user
 -- ----------------------------
-INSERT INTO `djmall_auth_user` VALUES ('1', 'zs', 'f0c69fc5845531fe3d42c98029480285', '', 'c8c47d3eff969778b420b105c931e8d9', '', '', '1');
-INSERT INTO `djmall_auth_user` VALUES ('2', 'aa', 'f0c69fc5845531fe3d42c98029480285', '', 'c8c47d3eff969778b420b105c931e8d9', '', '', '2');
-INSERT INTO `djmall_auth_user` VALUES ('3', 'ss', 'f0c69fc5845531fe3d42c98029480285', '', 'c8c47d3eff969778b420b105c931e8d9', '', '', '1');
-INSERT INTO `djmall_auth_user` VALUES ('4', 'ab', 'f0c69fc5845531fe3d42c98029480285', '', 'c8c47d3eff969778b420b105c931e8d9', '', '', '2');
-INSERT INTO `djmall_auth_user` VALUES ('5', 'aaa', 'f0c69fc5845531fe3d42c98029480285', '', 'c8c47d3eff969778b420b105c931e8d9', '15211111111', '1@163.com', '1');
-INSERT INTO `djmall_auth_user` VALUES ('6', 'aa1', '36693c5479c7069d532baa1471b407c4', '', '60022e38e2b2e307b6a88fa330666c1c', '15211111112', '2@163.com', '1');
+INSERT INTO `djmall_auth_user` VALUES ('1', 'admin', 'd0da592ee282f861da0e90f7fe0aa885', 'aa', 'a20fd84c6b2b9bf13b8e917e5e9648d9', '15211111111', '1@163.com', 'MAN');
+INSERT INTO `djmall_auth_user` VALUES ('2', 'yonghu', '9030f49e7b3a92f2d6fbab3bf428a9e6', 'ss', '2604a65335ca5daeebbe116d60390883', '15211111112', '2@163.com', 'WOMAN');
+INSERT INTO `djmall_auth_user` VALUES ('3', 'sh01', '37f7bf02910d7ca27b9c08befa6acac2', 'bb', '5abd6b762fa69107f78527158562305d', '15211111113', '3@163.com', 'MAN');
+INSERT INTO `djmall_auth_user` VALUES ('4', 'yh01', '5640abe011632d1807bfa55526b2c9e7', 'mmm', '9b9f345985b5d24ffe29fe11e71cf86a', '15211111114', '4@163.com', 'MAN');
